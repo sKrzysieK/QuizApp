ec143bd3a412816f10cf4363b26f207c4fea5aca
sKrzysieK <skrzysiek16.9@gmail.com>
2022-09-24 17:06:01 +0200

    repairing

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+
 const serveStatic = require("serve-static")
+const history = require('connect-history-api-fallback');
 const path = require('path');
-app = express();
+
+
+const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(history());    
 const port = process.env.PORT || 3000;
 app.listen(port);
